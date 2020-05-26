c985adcc6592efb04021f93d33fb28a10035fbf0
f4bjh <f4bjh@am-1002b56a9a9b.intern>
2020-05-27 00:03:14 +0200

    Alors...en effet, la table de calibration NE PEUT PAS rentrer dans la flash du PIC. Car cette table fait : 4095x12bits...soit près de 6ko. On la supprime donc, et on passe directement la valeur de l'ADC à la routine de conversion ASCII #142 supprime temporairement le driver de flash aussi

--- src/sw/calc/calc.asm
+++ src/sw/calc/calc.asm
@@ -1,16 +1,13 @@
 	include "p18f1320.inc" ;include the defaults for the chip
 
 	udata
-v_flh_offset_addr res 2
-v_flh_read res 2
 v_adcfwd_mV res 2
 v_adcref_mV res 2
-v_calctmp res 1	
-	
+v_calctmp res 1
+
 	extern v_adcfwd
 	extern v_adcref
-	extern f_flh_readword
-	
+
 	code
 IFDEF TEST
 ;-----------------------------------------
@@ -21,78 +18,26 @@ IFDEF TEST
 ;	v_adcref (2bytes) : résultat de l'ADC AN1 sur 10 bits
 ;Paramètres sorties
 ;	v_adcfwd_mV (2bytes) : résultat de l'ADC en mV en hexa
-;	v_adcref_mV (2bytes) : résultat de l'ADC en mV en hexa 
+;	v_adcref_mV (2bytes) : résultat de l'ADC en mV en hexa
 ;Traitements
-;1. v_flh_offset_addr = v_adcfwd
-;   v_flh_offset_addr + 1 = v_adcfwd + 1
-;2 v_flh_offset_addr = 2*v_flh_offset_addr et propager la retenue
-;3. Lecture d'un octet en flash (f_flh_readword)
-;4.v_adcfwd_mV = v_flh_read
-;5.v_adcfwd_mV +1 = v_flh_read+1
-;6. v_flh_offset_addr = v_adcref
-;   v_flh_offset_addr + 1= v_adcref + 1
-;7. v_flh_offset_addr = 2*v_flh_offset_addr et propager la retenue
-;8. Lecture d'un octet en flash (f_flh_readword)
-;9.v_adcref_mV = v_flh_read
-;10.v_adcref_mV +1 = v_flh_read+1	
 ;-----------------------------------------
 f_calc_adcmV
-	movf v_adcfwd+1,W
-	movwf v_flh_offset_addr+1
-	movlw 0x02
-	mulwf v_flh_offset_addr+1
-	movf PRODL,W
-	movwf v_flh_offset_addr+1
-	movf PRODH,W
-	movwf v_calctmp
-	
 	movf v_adcfwd,W
-	movwf v_flh_offset_addr
-	movlw 0x02
-	mulwf v_flh_offset_addr
-	movf PRODL,W
-	addwf v_calctmp,W
-	movwf v_flh_offset_addr
-	
-	call f_flh_readword
-
-	movf v_flh_read,W
 	movwf v_adcfwd_mV
-	movf v_flh_read+1,W
+	movf v_adcfwd+1,W
 	movwf v_adcfwd_mV+1
-f_calc_adcmV_6
-	movf v_adcref+1,W
-	movwf v_flh_offset_addr+1
-	movlw 0x02
-	mulwf v_flh_offset_addr+1
-	movf PRODL,W
-	movwf v_flh_offset_addr+1
-	movf PRODH,W
-	movwf v_calctmp
-	
-	movf v_adcref,W
-	movwf v_flh_offset_addr
-	movlw 0x02
-	mulwf v_flh_offset_addr
-	movf PRODL,W
-	addwf v_calctmp,W
-	movwf v_flh_offset_addr
-	
-	call f_flh_readword
 
-	movf v_flh_read,W
+	movf v_adcref,W
 	movwf v_adcref_mV
-	movf v_flh_read+1,W
+	movf v_adcref+1,W
 	movwf v_adcref_mV+1
 	return
 ENDIF
 
-IFDEF TEST	
+IFDEF TEST
 	global f_calc_adcmV
 ENDIF
-	global v_flh_offset_addr
-	global v_flh_read
 	global v_adcfwd_mV
 	global v_adcref_mV
-	
+
 	end
