eb048fa1926a69cd27e154a1bbfd6e6a0992d89c
Fabrice DECROP LONGET <52258351+f4bjh@users.noreply.github.com>
2020-06-10 16:20:17 +0200

    table de calibration pour le fw de TEST #207

--- src/tools/gen_th_cal/gen_cal_data.TEST.c
+++ src/tools/gen_th_cal/gen_cal_data.TEST.c
@@ -2,20 +2,42 @@
 
 int main (void)
 {
-  int i=0;
+  int i=0,j=0;
   char *filename="eep_ext.asm";
   FILE *fp;
 
   fp = fopen(filename,"w");
 
-  for (i=0;i<8;i++) {
-    printf("    DE 0x%02X\n",((i&(0xFF00))>>8));
-    fprintf(fp,"    DE 0x%02X\n",((i&(0xFF00))>>8)); 
-	   printf("    DE 0x%02X\n",(i&(0xFF)));
-    fprintf(fp,"    DE 0x%02X\n",(i&(0xFF))); 
+  
+  //FWD
+  for (i=0;i<4096;i++) {
+    printf("    DATA 0x%02X\n",((i&(0xFF00))>>8));
+    fprintf(fp,"    DATA 0x%02X\n",((i&(0xFF00))>>8)); 
+	   printf("    DATA 0x%02X\n",(i&(0xFF)));
+    fprintf(fp,"    DATA 0x%02X\n",(i&(0xFF))); 
 
   }
 
+  //REF
+  for (i=0;i<4096;i++) {
+    printf("    DATA 0x%02X\n",((i&(0xFF00))>>8));
+    fprintf(fp,"    DATA 0x%02X\n",((i&(0xFF00))>>8)); 
+	   printf("    DATA 0x%02X\n",(i&(0xFF)));
+    fprintf(fp,"    DATA 0x%02X\n",(i&(0xFF))); 
+
+  }
+
+  //FWD/REF
+  for (i=0;i<32;i++) {
+	for (j=1;j<128;j++) {
+    printf("    DATA 0x%02X\n",(((i/j)&(0xFF00))>>8));
+    fprintf(fp,"    DATA 0x%02X\n",(((i/j)&(0xFF00))>>8)); 
+	   printf("    DATA 0x%02X\n",((i/j)&(0xFF)));
+    fprintf(fp,"    DATA 0x%02X\n",((i/j)&(0xFF))); 
+	}
+  }
+  
+  
   fprintf(fp,"    end"); 
 
   fclose(fp);
