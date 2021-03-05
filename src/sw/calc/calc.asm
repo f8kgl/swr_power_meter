746bb8bb43c0dd92955fb79ad012659a8a95304c
f4bjh <f4bjh_95@yahoo.fr>
2021-03-05 16:49:04 +0100

    intégration d'un multiplieur 16x16
    #353

--- src/sw/calc/calc.asm
+++ src/sw/calc/calc.asm
@@ -1,59 +1,106 @@
 	include "p18f1320.inc" ;include the defaults for the chip
   include "calc.inc"
-
+	include "ltc2305.inc"
 
 
 	udata
-v_adcfwd_mV res 2
-v_adcref_mV res 2
-v_calc_n res 1
-v_calc_n_fwd res 1
-v_calc_n_ref res 1
-IFDEF TEST
-v_calc_count res 1
-v_calc_adc res 2
-ENDIF
-v_calctmp res 2
-v_calc_fwd res 2
-v_calc_ref res 2
+v_adc res 2
+v_adc_mV res 2
+v_adc_fs res 2
 
 	extern v_adcfwd
 	extern v_adcref
 
 	code
 IFDEF TEST
+;**********************************************************************************************
+;**********************************************************************************************
+;       
+;       16x16 Bit Unsigned Fixed Point Multiply 16 x 16 -> 32
+;
+;       Input:  16 bit unsigned fixed point multiplicand in AARGB0, AARGB1
+;               16 bit unsigned fixed point multiplier in BARGB0, BARGB1
+;
+;       Use:    CALL    FXM1616U
+;
+;       Output: 32 bit unsigned fixed point product in AARGB0, AARGB1, 
+;               AARGB2, AARGB3
+;
+;       Result: AARG  <--  AARG * BARG
+;
+;       Max Timing:     26 clks
+;
+;       Min Timing:     26 clks
+;
+;       PM: 25              DM: 7
+;
+;
+f_calc_mul1616U	
+		MOVPF	AARGB1,TEMPB1	
+
+		MOVFP	AARGB1,WREG
+		MULWF	BARGB1
+		MOVPF	PRODH,AARGB2
+		MOVPF	PRODL,AARGB3
+		
+		MOVFP	AARGB0,WREG
+		MULWF	BARGB0
+		MOVPF	PRODH,AARGB0
+		MOVPF	PRODL,AARGB1
+
+		MULWF	BARGB1
+		MOVPF	PRODL,WREG
+		ADDWF	AARGB2,F
+		MOVPF	PRODH,WREG
+		ADDWFC	AARGB1,F
+		CLRF	WREG,F
+		ADDWFC	AARGB0,F
+
+		MOVFP	TEMPB1,WREG
+		MULWF	BARGB0
+		MOVPF	PRODL,WREG
+		ADDWF	AARGB2,F
+		MOVPF	PRODH,WREG
+		ADDWFC	AARGB1,F
+		CLRF	WREG,F
+		ADDWFC	AARGB0,F
+		
+		RETLW	0x00
+
+
+
 ;-----------------------------------------
 ;Fonction Convertir la mesure des ADC en mV
 ;Nom 		;
 ;Paramètres entrée
-;	v_adcfwd (2bytes) : résultat de l'ADC AN0 sur 10 bits
-;	v_adcref (2bytes) : résultat de l'ADC AN1 sur 10 bits
+;	v_adc résultat de l'ADC sur 12 bits
 ;Paramètres sorties
-;	v_adcfwd_mV (2bytes) : résultat de l'ADC en mV en hexa
-;	v_adcref_mV (2bytes) : résultat de l'ADC en mV en hexa
-;Traitements
-;-----------------------------------------
-f_calc_adc_hex_fwd_and_ref
-	movf v_adcfwd,W
-	movwf v_adcfwd_mV
-	movf v_adcfwd+1,W
-	movwf v_adcfwd_mV+1
+;	v_adc_mV (2bytes) : résultat de l'ADC en mV en hexa
 
-	movf v_adcref,W
-	movwf v_adcref_mV
-	movf v_adcref+1,W
-	movwf v_adcref_mV+1
+;Traitements
+;   1. FXM1616U (ADC,(5000)) : 
+;	2. décalage à droite de 12 bits
+;-----------------------------------------
+f_calc_conv_adc_mV
+	movlw V_ADC_FULL_SCALE_MSB
+	movwf v_adc_fs
+	movlw V_ADC_FULL_SCALE_LSB
+	movwf v_adc_fs+1
+	
+	movff v_adcfwd,v_adc
+	movff v_adcfwd+1,v_adc+1
+	
+	call f_calc_mul1616U
+	;faire idem sur la voie REF
+	
 	return
 ENDIF
 
 
 IFDEF TEST
-	global f_calc_adc_hex_fwd_and_ref
+	global f_calc_conv_adc_mV
+	global v_adc
+	global v_adc_mV
 ENDIF
-	global v_adcfwd_mV
-	global v_adcref_mV
-	global v_calc_n_fwd
-	global v_calc_n_ref
-
 
 	end
