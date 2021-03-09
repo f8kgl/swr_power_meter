ae88bdd7e4f37ed5cce608e68055d0fe9c59e1cc
f4bjh <f4bjh_95@yahoo.fr>
2021-03-09 22:42:09 +0100

    gros oups qd même...#362 #359 #353

--- src/sw/calc/calc.asm
+++ src/sw/calc/calc.asm
@@ -4,114 +4,65 @@
 
 
 	udata
-v_calc_mul res 4
-v_calc_fwd_mV res 2
-v_calc_adcfs res 2
-
-	extern v_adcfwd
-	extern v_adcref
+v_calc_aarg res 4
+v_calc_barg res 2
+v_calc_tmp res 1
 
 	code
 IFDEF TEST
-;**********************************************************************************************
-;**********************************************************************************************
-;
-;       16x16 Bit Unsigned Fixed Point Multiply 16 x 16 -> 32
-;
-;       Input:  16 bit unsigned fixed point multiplicand in v_calc_mul+0, v_calc_mul+1
-;               16 bit unsigned fixed point multiplier in v_calc_adcfs+0, v_calc_adcfs+1
-;
-;       Use:    CALL    FXM1616U
-;
-;       Output: 32 bit unsigned fixed point product in v_calc_mul+0, v_calc_mul+1,
-;               v_calc_mul+2, v_calc_mul+3
-;
-;       Result: AARG  <--  AARG * BARG
-;
-;       Max Timing:     26 clks
-;
-;       Min Timing:     26 clks
-;
-;       PM: 25              DM: 7
-;
-;
-f_calc_mul1616U
-IF 0
-		MOVPF	v_calc_mul+1,TEMPB1
+  ;**********************************************************************************************
+  ;**********************************************************************************************
+  ;
+  ;       16x16 Bit Unsigned Fixed Point Multiply 16 x 16 -> 32
+  ;
+  ;       Input:  16 bit unsigned fixed point multiplicand in v_calc_aarg+0, v_calc_aarg+1
+  ;               16 bit unsigned fixed point multiplier in v_calc_barg+0, v_calc_barg+1
+  ;
+  ;       Result: AARG  <--  AARG * BARG
+  ;**********************************************************************************************
+  ;**********************************************************************************************
+f_calc_fxm1616u
+	    movff	v_calc_aarg+1,v_calc_tmp
 
-		MOVFP	v_calc_mul+1,WREG
-		MULWF	v_calc_adcfs+1
-		MOVPF	PRODH,v_calc_mul+2
-		MOVPF	PRODL,v_calc_mul+3
+  		movf	v_calc_aarg+1,W
+  		MULWF	v_calc_barg+1
+  		movff	PRODH,v_calc_aarg+2
+  		movff	PRODL,v_calc_aarg+3
 
-		MOVFP	v_calc_mul+0,WREG
-		MULWF	v_calc_adcfs+0
-		MOVPF	PRODH,v_calc_mul+0
-		MOVPF	PRODL,v_calc_mul+1
+  		movf	v_calc_aarg,W
+  		MULWF	v_calc_barg
+  		movff	PRODH,v_calc_aarg
+  		movff	PRODL,v_calc_aarg+1
 
-		MULWF	v_calc_adcfs+1
-		MOVPF	PRODL,WREG
-		ADDWF	v_calc_mul+2,F
-		MOVPF	PRODH,WREG
-		ADDWFC	v_calc_mul+1,F
-		CLRF	WREG,F
-		ADDWFC	v_calc_mul+0,F
+  		MULWF	v_calc_barg+1
+  		movf	PRODL,W
+  		ADDWF	v_calc_aarg+2,F
+  		movf	PRODH,W
+  		ADDWFC	v_calc_aarg+1,F
+      movlw 0x00
+  		ADDWFC	v_calc_aarg,F
 
-		MOVFP	TEMPB1,WREG
-		MULWF	v_calc_adcfs+0
-		MOVPF	PRODL,WREG
-		ADDWF	v_calc_mul+2,F
-		MOVPF	PRODH,WREG
-		ADDWFC	v_calc_mul+1,F
-		CLRF	WREG,F
-		ADDWFC	v_calc_mul+0,F
+  		movf	v_calc_tmp,W
+  		MULWF	v_calc_barg
+  		movf	PRODL,W
+  		ADDWF	v_calc_aarg+2,F
+  		movf	PRODH,W
+  		ADDWFC	v_calc_aarg+1,F
+      movlw 0x00
+  		ADDWFC	v_calc_aarg,F
 
-		RETLW	0x00
-ENDIF
-	return
-
-
-;-----------------------------------------
-;Fonction Convertir la mesure des ADC en mV
-;Nom 		;
-;Paramètres entrée
-;	v_adc résultat de l'ADC sur 12 bits
-;Paramètres sorties
-;	v_adc_mV (2bytes) : résultat de l'ADC en mV en hexa
-
-;Traitements
-;   1. FXM1616U (ADC,(5000)) :
-;	2. décalage à droite de 12 bits
-;-----------------------------------------
-f_calc_conv_adc_mV
-	movlw V_ADC_FULL_SCALE_MSB
-	movwf v_calc_adcfs
-	movlw V_ADC_FULL_SCALE_LSB
-	movwf v_calc_adcfs+1
-
-	movlw 0x00
-	movwf v_calc_mul
-	movwf v_calc_mul+1
-	movff v_adcfwd,v_calc_mul+2
-	movff v_adcfwd+1,v_calc_mul+3
-
-	call f_calc_mul1616U
-	;déclagae à droite de 12 bits
-	;transfère dans v_calc_fwd_mV
-	movff v_calc_mul+1,v_calc_fwd_mV+1
-	movff v_calc_mul,v_calc_fwd_mV
+  		return
 
 
 
-	;faire idem sur la voie REF
 
-	return
 ENDIF
 
 
 IFDEF TEST
-	global f_calc_conv_adc_mV
-	global v_calc_fwd_mV
+	global v_calc_aarg
+	global v_calc_barg
+	global f_calc_fxm1616u
 ENDIF
 
 	end
--- src/sw/calc/conv.asm
+++ src/sw/calc/conv.asm
@@ -7,9 +7,10 @@ v_calc_bin_mv_in res 2
 v_calc_bcd_out res 2
 v_calc_bcd_count res 1
 v_calc_mul_out res 6
-v_calc_aarg res 4
-v_calc_barg res 2
-v_calc_tmp res 1
+
+	extern v_calc_aarg
+	extern v_calc_barg
+  extern f_calc_fxm1616u
 
 	code
 ;-----------------------------------------
@@ -162,49 +163,6 @@ _f_calc_conv_mv_to_bcd
 	movff v_calc_bcd_out,POSTINC1
 	movff v_calc_bcd_out+1,POSTINC1
 	return
-
-  ;**********************************************************************************************
-  ;**********************************************************************************************
-  ;
-  ;       16x16 Bit Unsigned Fixed Point Multiply 16 x 16 -> 32
-  ;
-  ;       Input:  16 bit unsigned fixed point multiplicand in v_calc_aarg+0, v_calc_aarg+1
-  ;               16 bit unsigned fixed point multiplier in v_calc_barg+0, v_calc_barg+1
-  ;
-  ;       Result: AARG  <--  AARG * BARG
-  ;**********************************************************************************************
-  ;**********************************************************************************************
-f_calc_fxm1616u
-	    movff	v_calc_aarg+1,v_calc_tmp
-
-  		movf	v_calc_aarg+1,W
-  		MULWF	v_calc_barg+1
-  		movff	PRODH,v_calc_aarg+2
-  		movff	PRODL,v_calc_aarg+3
-
-  		movf	v_calc_aarg,W
-  		MULWF	v_calc_barg
-  		movff	PRODH,v_calc_aarg
-  		movff	PRODL,v_calc_aarg+1
-
-  		MULWF	v_calc_barg+1
-  		movf	PRODL,W
-  		ADDWF	v_calc_aarg+2,F
-  		movf	PRODH,W
-  		ADDWFC	v_calc_aarg+1,F
-      movlw 0x00
-  		ADDWFC	v_calc_aarg,F
-
-  		movf	v_calc_tmp,W
-  		MULWF	v_calc_barg
-  		movf	PRODL,W
-  		ADDWF	v_calc_aarg+2,F
-  		movf	PRODH,W
-  		ADDWFC	v_calc_aarg+1,F
-      movlw 0x00
-  		ADDWFC	v_calc_aarg,F
-
-  		return
 
 
 f_calc_conv_bin_to_mV
