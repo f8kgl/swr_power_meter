fbf8b001fecc9f28e7465677c338fd69c5fbe2cb
f4bjh <f4bjh_95@yahoo.fr>
2021-03-12 09:42:55 +0100

    mise en oeuvre d'un octet en EEP pour activation d'un niveau de trace en fonction du tag
    #380

--- src/sw/log/log.asm
+++ src/sw/log/log.asm
@@ -9,6 +9,7 @@ v_log_data_size res 1
 v_log_tag res 1
 v_log_nb_byte_to_write res 1
 v_log_addr res 1
+v_log_tmp res 1
 
 	extern v_eep_int_byte_to_write
 	extern f_eep_int_writebyte
@@ -21,8 +22,37 @@ _f_log_get_next_addr
 	call f_eep_int_readbyte
 	movwf v_log_addr
 	return
+	
+_f_log_tag_is_enabled
+;W=0 si le tag est activé dans l'octet de config des trace en EEP
+	movff v_log_tag,v_log_tmp
+	movlw ADDR_EEP_NEXT_ADDR_TO_WRITE
+	call f_eep_int_readbyte
+	
+	;w contient la config
+	;v_log_tmp contient le tag
+	xorwf v_log_tmp,f
+	btfsc STATUS,Z
+	goto _f_log_tag_is_enabled2	;Z=1 => STOP
+
+	movff v_log_tag,v_log_tmp
+	iorwf v_log_tmp,f
+	subwf v_log_tmp,f
+	btfsc STATUS,Z
+	goto _f_log_tag_is_enabled3	;Z=1; il faut activer la trace
+
+_f_log_tag_is_enabled2
+	movlw D'01'
+	goto _f_log_tag_is_enabled_end
+_f_log_tag_is_enabled3
+	clrf W
+_f_log_tag_is_enabled_end
+	return
 
 f_log_write
+  call _f_log_tag_is_enabled
+  tstfsz w
+  goto _f_log_write_loop_end2
   call _f_log_get_next_addr
   movlw NB_BYTE
   movwf v_log_nb_byte_to_write
@@ -73,7 +103,7 @@ _f_log_write_loop_end
   movlw ADDR_EEP_NEXT_ADDR_TO_WRITE
   call f_eep_int_writebyte
 
-
+_f_log_write_loop_end2
   return
 
 
