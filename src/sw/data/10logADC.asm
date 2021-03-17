5780ec7f48b6ee6ba7968c670a4c297c3969e4b0
f4bjh <f4bjh_95@yahoo.fr>
2021-03-17 12:01:06 +0100

    génération de la table de 10*log(ADC) #347

--- src/sw/data/10logADC.asm
+++ src/sw/data/10logADC.asm
@@ -5,8 +5,11 @@
 	code
 
 c_10logADC_table
+	include "10logADC.inc"
+IF 0
 	DW 0xDEAD
 	DW 0xBEEF
+ENDIF
 
 	global c_10logADC_table
 
--- src/tools/gen_th_cal/gen_10logADC_table.c
+++ src/tools/gen_th_cal/gen_10logADC_table.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdint.h>
 
 int main (void)
 {
   int i=0;
   double ln=0;
-  char *filename="10logADC.asm";
+  uint16_t lut=0;
+  char *filename="10logADC.inc";
   FILE *fp;
 
   fp = fopen(filename,"w");
 
   
-  for (i=1;i<2048;i++)
+  for (i=1;i<=2048;i++)
   {
-	ln = 10*log(i);
-	printf(ADC=0x%03X - 10*log(ADC) = %g\n",i,ln);
+    ln = 10*log10(i);
+    lut  = (int) round(10*ln);
+    printf("ADC=0x%04X(%d) - 10*log(ADC) = %.1fdB (%d 0x%04X) - DW 0x%04X\n",i,i,ln,lut, lut,lut);
+    fprintf(fp,"    DW 0x%04X\n",lut);
   }
-  
-  fprintf(fp,"    end"); 
-
+ 
   fclose(fp);
 
   return 0;
